8467505077935f5cedde4371ffcce19f28fcf85f
Redd Forfieda <redd.forfieda@stu.ocsb.ca>
2023-12-19 12:22:05 -0500

    added cpt-01

--- code.py
+++ code.py
@@ -1,1 +1,22 @@
-print("Hello Redd")
+#!/usr/bin/env python3
+
+# Created by: Redd Forfieda
+# Created on:december 19 2023
+# This program is the "space Alien" program on the pybadge
+
+import ugame
+import stage
+
+
+def game_scene():
+    #this function is the main game game_scene
+    print("\n\n\n") # 3 blank lines
+    print("hello, Redd!")
+    
+    # repeat forever, game loop
+    while True:
+        pass # just a placeholder for now
+
+
+if __name__  == "__main__":
+    game_scene()     
